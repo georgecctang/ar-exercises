e40809e148935989004d85395e3b3974786e751f
George Tang <georgecctang@gmail.com>
2020-11-09 20:39:03 +0000

    complete exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,10 +5,9 @@ puts "----------"
 
 # Your code goes below here ...
 
-store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true, created_at: Time.now.getutc, updated_at: Time.now.getutc)
-
-store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true, created_at: Time.now.getutc, updated_at: Time.now.getutc)
-
-store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false, created_at: Time.now.getutc, updated_at: Time.now.getutc)
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true, created_at: Time.now.getutc, updated_at: Time.now.getutc)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true, created_at: Time.now.getutc, updated_at: Time.now.getutc)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false, created_at: Time.now.getutc, updated_at: Time.now.getutc)
 
 puts Store.count();
+# puts Store.count();
